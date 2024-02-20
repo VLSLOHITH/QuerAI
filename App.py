7df80c295e79b57fcccd89abfca5a41f69568a2d
VLS_LOHITH <84633352+VLSLOHITH@users.noreply.github.com>
2024-02-20 13:54:33 +0530

    Update App.py

--- App.py
+++ App.py
@@ -15,31 +15,31 @@ selected=st.sidebar.selectbox("Type of document",options=["pdf_file","Webpage","
 query=st.text_input(label="Ask Your Query?",max_chars=50)
 
 try:
-if selected=="pdf_file":
-    uploaded=st.sidebar.file_uploader("Choose Your PDF File",type="pdf")
-    if uploaded and submitted:
-        data=mn.pdf_loader(uploaded)
-        mn.Str_vector_store(data)
-elif selected=="Webpage":
-    uploaded=st.sidebar.text_input(label="Paste Your URL")
-    if uploaded and submitted:
-        data=mn.url_loader(uploaded)
-        mn.Doc_vector_store(data)
-elif selected=="Youtube Video":
-    uploaded=st.sidebar.text_input(label="Paste the url of the Youtube Video")
-    if uploaded and submitted:
-        data=mn.Youtube_loader(uploaded)
-        mn.Doc_vector_store(data)
+    if selected=="pdf_file":
+        uploaded=st.sidebar.file_uploader("Choose Your PDF File",type="pdf")
+        if uploaded and submitted:
+            data=mn.pdf_loader(uploaded)
+            mn.Str_vector_store(data)
+    elif selected=="Webpage":
+        uploaded=st.sidebar.text_input(label="Paste Your URL")
+        if uploaded and submitted:
+            data=mn.url_loader(uploaded)
+            mn.Doc_vector_store(data)
+    elif selected=="Youtube Video":
+        uploaded=st.sidebar.text_input(label="Paste the url of the Youtube Video")
+        if uploaded and submitted:
+            data=mn.Youtube_loader(uploaded)
+            mn.Doc_vector_store(data)
+            
+        if not(uploaded):
+            st.info("Ensure you upload your source first; check the sidebar for guidance.",icon="ℹ️")
+        search=st.button("search")
         
-    if not(uploaded):
-        st.info("Ensure you upload your source first; check the sidebar for guidance.",icon="ℹ️")
-    search=st.button("search")
-    
-    if selected and uploaded and query and os.path.exists("__pycache__/main.cpython-311.pyc") and search:
-        if selected!="pdf_file" :
-            st.write(mn.doc_Query(query))
-        else:
-            st.write(mn.pdf_Query(query))
+        if selected and uploaded and query and os.path.exists("__pycache__/main.cpython-311.pyc") and search:
+            if selected!="pdf_file" :
+                st.write(mn.doc_Query(query))
+            else:
+                st.write(mn.pdf_Query(query))
 except ValueError :
     st.error("Please,give the correct formate of source.",icon="❗")
 except IndexError:

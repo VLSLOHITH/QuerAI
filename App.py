e0292dac08cf1ca4c22a07f6612c24beaed1e33a
VLS_LOHITH <84633352+VLSLOHITH@users.noreply.github.com>
2024-02-20 05:55:09 +0530

    Update App.py

--- App.py
+++ App.py
@@ -10,7 +10,7 @@ uploaded=None
 # form=st.sidebar.form(key="my_form")
 
 submitted=st.sidebar.button("submit")
-st.info("please upload your source before asking the questions",icon="i")
+st.info("please upload your source before asking the questions",icon=i)
 selected=st.sidebar.selectbox("Type of document",options=["pdf_file","Webpage","Youtube Video"],index=None)
 
 query=st.text_input(label="Ask Your Query?",max_chars=50)
